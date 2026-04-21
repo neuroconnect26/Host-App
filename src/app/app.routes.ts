ebb8715af4a7a5640c6d350c7920a4075d3983fe
sabairfan <sabaaairfan@gmail.com>
2026-04-20 22:53:46 -0400

    Finalizing host app integration for Milestone 6

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'glucose',
+    loadComponent: () => loadRemoteModule('glucose', './Component').then(m => m.AppComponent)
+  },
+  {
+    path: 'cholesterol',
+    loadComponent: () => loadRemoteModule('cholesterol', './Component').then(m => m.AppComponent)
+  },
+  {
+    path: 'sleep',
+    loadComponent: () => loadRemoteModule('sleep', './Component').then(m => m.AppComponent)
+  },
+  {
+    path: 'mood',
+    loadComponent: () => loadRemoteModule('mood', './Component').then(m => m.AppComponent)
+  },
+  {
+    path: 'heart-rate',
+    loadComponent: () => loadRemoteModule('heartRate', './Component').then(m => m.AppComponent)
+  }
+];
