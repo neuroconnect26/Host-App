8c51ab59fc6a02fddac947b38b0447f3bbd22014
Stacey <staceyg176@gmail.com>
2026-04-21 14:56:05 -0400

    Auto-generated commit

---
+++ main-L3V4PTV5.js
@@ -0,0 +1,1 @@
+import{a as r}from"./chunk-AWOANDKJ.js";r({mfe1:"http://localhost:3000/remoteEntry.json"}).catch(o=>console.error(o)).then(o=>import("./chunk-GWZX5ONC.js")).catch(o=>console.error(o));
