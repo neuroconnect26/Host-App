db41723502a3538d53325082f75f5ff321dabe9c
sabairfan <sabaaairfan@gmail.com>
2026-04-20 23:13:22 -0400

    Finalized Milestone 6 Host Platform with all 5 routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,22 +4,27 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'glucose',
-    loadComponent: () => loadRemoteModule('glucose', './Component').then(m => m.AppComponent)
+    loadComponent: () => 
+      loadRemoteModule('glucose', './Component').then(m => m.AppComponent)
   },
   {
     path: 'cholesterol',
-    loadComponent: () => loadRemoteModule('cholesterol', './Component').then(m => m.AppComponent)
+    loadComponent: () => 
+      loadRemoteModule('cholesterol', './Component').then(m => m.AppComponent)
   },
   {
     path: 'sleep',
-    loadComponent: () => loadRemoteModule('sleep', './Component').then(m => m.AppComponent)
+    loadComponent: () => 
+      loadRemoteModule('sleep', './Component').then(m => m.AppComponent)
   },
   {
     path: 'mood',
-    loadComponent: () => loadRemoteModule('mood', './Component').then(m => m.AppComponent)
+    loadComponent: () => 
+      loadRemoteModule('mood', './Component').then(m => m.AppComponent)
   },
   {
     path: 'heart-rate',
-    loadComponent: () => loadRemoteModule('heartRate', './Component').then(m => m.AppComponent)
+    loadComponent: () => 
+      loadRemoteModule('heartRate', './Component').then(m => m.AppComponent)
   }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,13 @@
 import { Component } from '@angular/core';
-import { HeartComponent } from './heart/heart';
+import { RouterModule } from '@angular/router'; // 1. Add this import
 
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   imports: [HeartComponent],
-//   template: ./app.css,
-// })
-  
 @Component({
   selector: 'app-root',
-  imports: [HeartComponent],
   standalone: true,
- templateUrl: './app.html',
+  imports: [RouterModule], // 2. Add RouterModule here
+  templateUrl: './app.html',
   styleUrl: './app.css'
 })
-  
-export class App {
+export class AppComponent {
+  title = 'my-app';
 }
