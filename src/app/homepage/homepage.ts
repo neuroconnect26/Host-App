1b933f94e27b11713a5bdc76322e1efa332cf6ad
neuroconnect26 <neuroconnect26@gmail.com>
2026-05-12 23:28:30 -0400

    Update homepage.ts

--- src/app/homepage/homepage.ts
+++ src/app/homepage/homepage.ts
@@ -64,7 +64,7 @@ export class Homepage implements OnInit, OnDestroy {
     this.sleepCard = undefined;
     this.glucoseCard = undefined;
     this.moodCard = undefined;
-    //this.heartRateCard = undefined;
+    this.heartRateCard = undefined;
   }
 
   getGreeting(): string {
